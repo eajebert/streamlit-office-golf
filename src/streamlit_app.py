72527761cfa0b5d81df606ad5bd47386aff7aea7
Elliot Ebert <elliot@clippd.io>
2022-04-15 17:47:04 +0100

    change structure.

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
-with st.sidebar():
+with st.sidebar:
     n_holes = st.number_input("No. of hole played", min_value=1)
     dropdown_options = [f"Hole {hole}" for hole in n_holes]
     st.selectbox("Select hole to edit:", dropdown_options)
