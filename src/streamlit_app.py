2eeddddf91da857431ee4f26a3b32bf708ca48ee
Elliot Ebert <elliot@clippd.io>
2022-04-15 17:52:07 +0100

    debug

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 with st.sidebar:
     n_holes = int(st.number_input("No. of hole played", min_value=1))
-    format_hole = lambda x: f"Hole {x + 1}"
+    format_hole = lambda x: f"Hole {x}"
     st.selectbox("Select hole to edit:", range(1, n_holes+1), format_func=format_hole)
 
+# It's a variable that doesn't exist.
 # st.number_of_shots
